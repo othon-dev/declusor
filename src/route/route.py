c1c9b202e486db528a723129d2a0535f94146dbe
othon <162609004+othonhugo@users.noreply.github.com>
2025-09-23 21:52:00 -0300

    refactor: rename 'controllers' package to 'controller'

--- src/route/route.py
+++ src/route/route.py
@@ -1,12 +1,12 @@
-import controllers
+import controller
 from interfaces import IRouter
 
 
 def set_routes(router: IRouter) -> None:
-    router.connect("load", controllers.call_load)
-    router.connect("command", controllers.call_command)
-    router.connect("shell", controllers.call_shell)
-    router.connect("upload", controllers.call_upload)
-    router.connect("execute", controllers.call_execute)
-    router.connect("help", controllers.call_help)
-    router.connect("exit", controllers.call_exit)
+    router.connect("load", controller.call_load)
+    router.connect("command", controller.call_command)
+    router.connect("shell", controller.call_shell)
+    router.connect("upload", controller.call_upload)
+    router.connect("execute", controller.call_execute)
+    router.connect("help", controller.call_help)
+    router.connect("exit", controller.call_exit)
