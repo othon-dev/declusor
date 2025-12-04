0e0f75050d1734af29539ab795b1c658c14e0c50
othon <162609004+othonhugo@users.noreply.github.com>
2025-12-04 12:01:21 -0300

    reactor(controller): refactor `help` controller argument parsing to use `parse_command_arguments`

--- src/declusor/controller/help.py
+++ src/declusor/controller/help.py
@@ -1,10 +1,14 @@
+from typing import Optional
+
 from declusor import interface, util
 
 
 async def call_help(session: interface.ISession, router: interface.IRouter, line: str) -> None:
     """Display help information about available commands."""
 
-    if line:
-        util.write_string_message(router.get_route_usage(line))
+    arguments, _ = util.parse_command_arguments(line, {"command": Optional[str]})
+
+    if arguments["command"]:
+        util.write_string_message(router.get_route_usage(arguments["command"]))
     else:
         util.write_string_message(router.documentation)
