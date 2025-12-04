dd51c677127270757d659313c10e42d012ebc61e
othon <162609004+othonhugo@users.noreply.github.com>
2025-12-03 23:26:03 -0300

    refactor(upload): refactor `call_upload` to leverage new `StoreFile` command

--- src/declusor/controller/upload.py
+++ src/declusor/controller/upload.py
@@ -1,20 +1,18 @@
-from base64 import b64encode
-
-from declusor.interface import IRouter, ISession
-from declusor.util import format_bash_function_call, load_file_safely, parse_command_arguments, write_binary_message
+from declusor import interface, util, core, schema
 
 
-async def call_upload(session: ISession, router: IRouter, line: str) -> None:
-    """Uploads a file to the target machine."""
+async def call_upload(session: interface.ISession, router: interface.IRouter, line: str) -> None:
+    """Upload a file to the target machine."""
 
-    arguments, _ = parse_command_arguments(line, {"filepath": str})
+    arguments, _ = util.parse_command_arguments(line, {"filepath": str})
 
-    if (file_content := load_file_safely(arguments["filepath"])) is None:
-        return
+    if (file_content := util.try_load_file(arguments["filepath"])) is None:
+        raise core.ControllerError("Failed to load the specified file.")
 
-    function_call = format_bash_function_call("store_base64_encoded_value", b64encode(file_content).decode())
+    file_base64 = util.convert_to_base64(file_content)
+    function_call = util.format_function_call(schema.STORE_FILE_FUNCTION, file_base64)
 
     await session.write(function_call.encode())
 
     async for data in session.read():
-        write_binary_message(data)
+        util.write_binary_data(data)
